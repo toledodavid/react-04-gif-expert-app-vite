3b2fb723b06bd8ecf1a1b2a347eedc204adcd4a9
David Toledo <dtoledoz0207@gmail.com>
2023-03-08 11:20:56 -0600

    Create a category list using useState hook in GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Vikings', 'Dragon ball']);
+
+  console.log(categories);
+
   return(
     <>
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      <ol>
+        {
+          categories.map((category, index) => (
+            <li key={index}>{category}</li>
+          ))
+        }
+      </ol>
     </>
   );
 }
