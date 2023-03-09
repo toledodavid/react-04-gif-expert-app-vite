211deb690a4df4141fab9f9be5daf40924756e05
David Toledo <dtoledoz0207@gmail.com>
2023-03-09 11:30:26 -0600

    Create GifGrid component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
@@ -18,16 +19,13 @@ export const GifExpertApp = () => {
     <>
       <h1>GifExpertApp</h1>
 
-      {/* Input */}
       <AddCategory onNewCategory={value => onAddCategory(value)} />
 
-      <ol>
-        {
-          categories.map((category) => (
-            <li key={category}>{category}</li>
-          ))
-        }
-      </ol>
+      {
+        categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))
+      }
     </>
   );
 }
