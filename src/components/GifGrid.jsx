6070dba15b2096f8d2e4adeb0d7b06e0150a526b
David Toledo <dtoledoz0207@gmail.com>
2023-03-10 09:35:41 -0600

    Show images titles in GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 
 export const GifGrid = ({category}) => {
 
+  const [images, setImages] = useState([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
   useEffect(() => {
-    getGifs();
+   getImages();
   }, []);
 
   return(
     <>
       <h3>{category}</h3>
-      <p>Hello world</p>
+      <ol>
+        {
+          images.map(({id, title}) => (
+            <li key={id}>{title}</li>
+          ))
+        }
+      </ol>
     </>
   );
 }
