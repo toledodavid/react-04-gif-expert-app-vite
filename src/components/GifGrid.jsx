bef693963b56a80883be1f0aa5ff723365e8e64e
David Toledo <dtoledoz0207@gmail.com>
2023-03-10 10:36:05 -0600

    Create custom hook useFetchGifs to handle the logic to getGifs from the API and handle a loading property

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
 import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 
 export const GifGrid = ({category}) => {
 
-  const [images, setImages] = useState([]);
-
-  const getImages = async() => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  }
-
-  useEffect(() => {
-   getImages();
-  }, []);
+  const {images, isLoading} = useFetchGifs(category);
 
   return(
     <>
