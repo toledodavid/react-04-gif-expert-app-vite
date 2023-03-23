d04ea63ff4da03c8c1fda37b3d4d5b546ca2f338
David Toledo <dtoledoz0207@gmail.com>
2023-03-23 11:58:43 -0600

    Add test for GifItem component to test img and p elements

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Tests for <GifItem />', () => {
@@ -11,4 +11,19 @@ describe('Tests for <GifItem />', () => {
     expect(container).toMatchSnapshot();
   });
 
+  test('should show the image with the correct URL and ALT', () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url);
+    // expect(screen.getByRole('img').alt).toBe(title);
+
+    const {src, alt} = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('should show the title', () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
 });
