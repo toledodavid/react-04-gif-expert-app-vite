662dd165de747331aae886c0e3aca892f3b2dbb3
David Toledo <dtoledoz0207@gmail.com>
2023-03-24 11:43:44 -0600

    Implement jest.fn() function to simulate a call function in AddCategory component

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -16,8 +16,9 @@ describe('Tests for <AddCategory />', () => {
 
   test('should call onNewCategory if input value has a value', () => {
     const inputValue = 'Nintendo';
+    const onNewCategory = jest.fn();
 
-    render(<AddCategory onNewCategory={() => {}} />);
+    render(<AddCategory onNewCategory={onNewCategory} />);
 
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form');
@@ -26,6 +27,24 @@ describe('Tests for <AddCategory />', () => {
     fireEvent.submit(form);
 
     expect(input.value).toBeFalsy();
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test('should not call onNewCategory if input value is empty', () => {
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, {target: {value: ''}});
+    fireEvent.submit(form);
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalled();
   });
 
 });
