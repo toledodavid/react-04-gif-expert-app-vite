cfad7c825663e8590343a94287723606f2692e6b
David Toledo <dtoledoz0207@gmail.com>
2023-03-29 10:31:03 -0600

    Add a complete mock for custom hook (useFetchGifs) in GifGrid test file

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,15 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
+
 
 describe('Tests for <GifGrid />', () => {
 
   const category = 'Xbox';
 
   test('should show loading at the beginnig', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
+
     render(<GifGrid category={category} />);
 
     expect(screen.getByText('Loading...')).toBeTruthy();
     expect(screen.getByText(category)).toBeTruthy();
+  });
+
+  test('should show items when useFetchGifs loads the images', () => {
+    const gifs = [
+      {
+        id: '123',
+        title: 'abc',
+        url: 'https://abc.com'
+      },
+      {
+        id: '098',
+        title: 'xyz',
+        url: 'https://xyz.com'
+      }
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole('img').length).toBe(2);
   });
 
 });
